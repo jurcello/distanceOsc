ca471b68ac5f7456340b042f37964c59cafc5e34
Jur de Vries <info@jurdevries.nl>
2020-12-13 10:33:38 +0100

    Created DistanceSensor class.

--- sampleOsc.py
+++ sampleOsc.py
@@ -5,35 +5,37 @@ from pythonosc import udp_client
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
+class DistanceSensor:
+    def __init__(self, gpio_trigger, gpio_echo):
+        self.gpio_trigger = gpio_trigger
+        self.gpio_echo = gpio_echo
+        self.last_distance = 0
+        GPIO.setup(self.gpio_trigger, GPIO.OUT)
+        GPIO.setup(self.gpio_echo, GPIO.IN)
 
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+    def measure(self):
+        GPIO.output(self.gpio_trigger, True)
 
+        time.sleep(0.00001)
+        GPIO.output(self.gpio_trigger, False)
 
-def distance():
-    GPIO.output(GPIO_TRIGGER, True)
-
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-
-    start_time = time.time()
-    stop_time = time.time()
-
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
         start_time = time.time()
-
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
         stop_time = time.time()
 
-    time_elapsed = stop_time - start_time
+        # save StartTime
+        while GPIO.input(self.gpio_echo) == 0:
+            start_time = time.time()
 
-    distance = (time_elapsed * 34300) / 2
+        # save time of arrival
+        while GPIO.input(self.gpio_echo) == 1:
+            stop_time = time.time()
 
-    return distance
+        time_elapsed = stop_time - start_time
+
+        self.last_distance = (time_elapsed * 34300) / 2
+
+
+sensor = DistanceSensor(gpio_trigger=18, gpio_echo=24)
 
 
 if __name__ == "__main__":
@@ -46,8 +48,8 @@ if __name__ == "__main__":
 
     try:
         while True:
-            dist = distance()
-            client.send_message("/hello_world", dist)
+            sensor.measure()
+            client.send_message("/hello_world", sensor.last_distance)
             time.sleep(0.02)
 
     except KeyboardInterrupt:
